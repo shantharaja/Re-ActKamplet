9bcae59817e2226fb87acbaa73bf1d7e431dd53b
shantharaja <shanthahlk@gmail.com>
2018-03-22 15:57:09 +0530

    new changes

--- Login.js
+++ Login.js
@@ -1,20 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Switch, Route, Link,Button} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import Welcome from './Welcome';
+import EasyTransition from 'react-easy-transition'
 
 class Login extends Component {
+		
+   
    render() {
       return (
-         <div>
-            <h2>Login</h2>
-			
-			Username : <input type="text" name="username" id="username"/><br/>
-			<br/>
-			Password : <input type="password" name="password" id="password"/><br/><br/>
-			
-			<button type="button">Login</button>
-			
-         </div>
+
+      	<Router>
+	         <div>
+	            <h2>Login</h2>
+				
+				Username : <input type="text" name="username" id="username"/><br/>
+				<br/>
+				 Password : <input type="password" name="password" id="password"/><br/><br/>
+				
+				<button ><Link style={divStyle} to={'/Welcome'}>Home</Link></button>
+				<Switch>
+						<Route path='/Welcome' component={Welcome}/>
+				</Switch>
+				
+	         </div>
+         </Router>
       );
    }
 }
+var divStyle = {
+  textDecorationLine: "",
+    textDecorationStyle: "solid",
+    textDecorationColor: "#000"
+};
 export default Login;
---
+++ Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Welcome extends Component {
+   render() {
+      return (
+         <div>
+            <h2>Welcome</h2>
+			
+			
+			
+         </div>
+      );
+   }
+}
+export default Welcome;
